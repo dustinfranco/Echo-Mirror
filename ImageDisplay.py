01b86773288ca94071678571d5d60138e4519e67
Dustin Franco <dustfranco@gmail.com>
2017-09-10 19:04:35 -0700

    added Image Display control file

---
+++ ImageDisplay.py
@@ -0,0 +1,7 @@
+from BrightnessControl import minToMaxScreenBrightness, maxToMinScreenBrightness
+from showImage import showImage
+import cv2
+
+while(1):
+    showImage("/users/dustinfranco/desktop/new_picture.jpg")
+    showImage("/users/dustinfranco/desktop/black.png")
