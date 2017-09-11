05df13b14daa187378c9dbea3d09866322e39cc6
Dustin Franco <dustfranco@gmail.com>
2017-09-10 20:10:29 -0700

    image display now shows the last picture taken rather than the current picture

--- ImageDisplay.py
+++ ImageDisplay.py
@@ -3,5 +3,5 @@ from showImage import showImage
 import cv2
 
 while(1):
-    showImage("/users/dustinfranco/desktop/new_picture.jpg")
+    showImage("/users/dustinfranco/desktop/old_picture.png")
     showImage("/users/dustinfranco/desktop/black.png")
