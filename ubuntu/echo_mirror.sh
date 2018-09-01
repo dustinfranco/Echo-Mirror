9b32c3f3fad1a24492b971fc4b22ebfc468e47ba
Dustin Franco <dustfranco@gmail.com>
2018-08-31 20:57:14 -0700

    added functionality to take rotated picture

--- ubuntu/echo_mirror.sh
+++ ubuntu/echo_mirror.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "beginning echo mirror"
+ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -ss 0:0:01 -frames 1 ~/echo-mirror/ubuntu/pics/out.png -y
+ffmpeg -i ~/echo-mirror/ubuntu/pics/out.png -vf transpose=2 ~/echo-mirror/ubuntu/pics/out.png -y
+
+
