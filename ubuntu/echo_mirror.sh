4b9e846c911146dd3ff988bae6ddf32a2e71225b
Dustin Franco <dustfranco@gmail.com>
2018-09-12 11:25:18 -0700

    added eog image showing, added fade screen off after displaying picture

--- ubuntu/echo_mirror.sh
+++ ubuntu/echo_mirror.sh
@@ -7,11 +7,29 @@ ffmpeg -i ~/echo-mirror/ubuntu/pics/out.png -vf transpose=2 ~/echo-mirror/ubuntu
 number_faces=`python face_detect_cv3.py`
 if [ "$number_faces" -gt 0 ]; then
   echo "faces found: $number_faces"
-  #fade screen on
+  #to disable eye of gnome animations:
+  #gsettings set org.gnome.desktop.interface enable-animations false
+  
+  #Display Image
+  eog --fullscreen ./pics/out.png &
+  
+  #Fade screen on
   for i in $(seq 0 0.01 1)
   do
     xrandr --output DP-1 --brightness $i
   done
+  sleep 3.0
+  
+  #Fade sreen off
+  for m in $(seq 0 0.01 1)
+  do
+    brightness=$(bc<<<1.0-$m)
+    #echo $brightness
+    xrandr --output DP-1 --brightness $brightness
+  done
+
+  xrandr --output DP-1 --brightness 1.0
+
 else
   echo "no faces"
 fi
