fb7e0dedf26f08fb7cba871d658cb1b3712b7b32
Dustin Franco <dustfranco@gmail.com>
2018-10-06 16:50:43 -0700

    mostly works but has bugs when in turnkey

--- ubuntu/echo_mirror.sh
+++ ubuntu/echo_mirror.sh
@@ -1,57 +1,76 @@
 #!/bin/bash
+mkdir ~/starting
+#eog --fullscreen ~/echo-mirror/pics/black.png &
+while :
+do
+  #rm /home/phnx/echo-mirror/ubuntu/pics/test.png
+  #xrandr --output DP-1 --brightness 0.0
+  #Take picture
+  ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -ss 0:0:01 -frames 1 ~/echo-mirror/ubuntu/pics/out_new.png -y
+  ffmpeg -i ~/echo-mirror/ubuntu/pics/out_new.png -vf transpose=0 ~/echo-mirror/ubuntu/pics/out.png -y
 
-#Take picture
-mv ./pics/out.png ./pics/out_last.png
-xrandr --output DP-1 --brightness 1.0
-ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -ss 0:0:01 -frames 1 ~/echo-mirror/ubuntu/pics/out.png -y
-ffmpeg -i ~/echo-mirror/ubuntu/pics/out.png -vf transpose=0 ~/echo-mirror/ubuntu/pics/out.png -y
+  #Analyze picture
+  #number_faces=`python face_detect_cv3.py`
+  python face_detect_cv3.py
+  number_faces=$?
 
-#Analyze picture
-number_faces=`python face_detect_cv3.py`
+  #no faces, return to beginning
+  if [ "$number_faces" -eq 0 ]; then
+    wall "no faces"
 
+  #found faces
+  else
+    #start recording
+    wall "faces found: $number_faces"
+        #to disable eye of gnome animations:
+        #gsettings set org.gnome.desktop.interface enable-animations false
+    
+    #Kill any other images 
+    kill `ps -ef| grep eog | awk '{print $2}'`
+    #obligatory "it doesn't work without a sleep"
+    sleep 0.1 
+    #Display new image
+    #nice -n -3 eog --fullscreen ./pics/out.gif &
+    mplayer -fs ~/echo-mirror/ubuntu/pics/out.gif &
+    sleep 1.0
+    
+    #TODO: start recording now
 
-#no faces, return to beginning
-if [ "$number_faces" -eq 0 ]; then
-  echo "no faces"
+    ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -t 15 ~/echo-mirror/ubuntu/pics/out_new.gif -y  &
+    #sleep 10.0
+    #Fade `screen on
+    for i in $(seq 0 0.01 1)
+    do
+      xrandr --output DP-1 --brightness $i
+    done
+    sleep 9.0
+    
+    #Fade sreen off
+    for m in $(seq 0 0.01 1)
+    do
+      brightness=$(bc<<<1.0-$m)
+      #echo $brightness
+      xrandr --output DP-1 --brightness $brightness
+    done
 
-#found faces
-else
-  #start recording
-  echo "faces found: $number_faces"
-      #to disable eye of gnome animations:
-      #gsettings set org.gnome.desktop.interface enable-animations false
-  
-  #Kill any other images 
-  kill `ps -ef| grep eog | awk '{print $2}'`
-  #obligatory "it doesn't work without a sleep"
-  sleep 0.1 
-  #Display new image
-  eog --fullscreen ./pics/out.png &
-  
-  #TODO: start recording now
+    #TODO: grayscale video
+    #TODO: replace old recording with new recording'
+    #ffmpeg -i ~/echo-mirror/ubuntu/pics/out_new.gif -t 15 -vf transpose=0 ~/echo-mirror/ubuntu/pics/out_new_2.gif -y
+    ffmpeg -i ~/echo-mirror/ubuntu/pics/out_new.gif -vf hflip -c:a copy ~/echo-mirror/ubuntu/pics/out_new_2.gif -y
 
-  #Fade screen on
-  for i in $(seq 0 0.01 1)
-  do
-    xrandr --output DP-1 --brightness $i
-  done
-  sleep 3.0
-  
-  #Fade sreen off
-  for m in $(seq 0 0.01 1)
-  do
-    brightness=$(bc<<<1.0-$m)
-    #echo $brightness
-    xrandr --output DP-1 --brightness $brightness
-  done
+    #ffmpeg -i ~/echo-mirror/ubuntu/pics/out_new.gif -t 15 -vf transpose=2 ~/echo-mirror/ubuntu/pics/out_new_2.gif -y
+    ffmpeg -i ~/echo-mirror/ubuntu/pics/out_new_2.gif -t 15 -vf hue=s=0 ~/echo-mirror/ubuntu/pics/out.gif -y
+    #gifsicle -O3 ~/echo-mirror/ubuntu/pics/out_new.gif ~/echo-mirror/ubuntu/pics/out.gif
 
-  #TODO: grayscale video
-  #TODO: replace old recording with new recording
-
-  kill `ps -ef| grep eog | awk '{print $2}'`
-  eog --fullscreen ./pics/black.png &
-  xrandr --output DP-1 --brightness 1.0
-  kill `ps -ef| grep eog | awk '{print $2}'`
-fi
+    #kill `ps -ef| grep eog | awk '{print $2}'`
+    #eog --fullscreen ~/echo-mirror/ubuntu/pics/test.png &
+    #kill `ps -ef| grep eog | awk '{print $2}'`
+    
+    #xrandr --output DP-1 --brightness 1.0
+    #echo "kill me now"
+    #sleep 5.0
+    #xrandr --output DP-1 --brightness 0.0
+  fi
+done
 
 
--- ubuntu/face_detect_cv3.py
+++ ubuntu/face_detect_cv3.py
@@ -8,7 +8,7 @@ path = "/Library/emp/"
 
 def findNumberOfFaces(imageName, inputScaleFactor = 1.1, rectangle = False, sizeRatio = 1.0, minXFaceSize = 15, minYFaceSize = 15):
     m = int(round(time.time() * 1000))
-    image = cv2.imread("./pics/out.png")
+    image = cv2.imread("/home/phnx/echo-mirror/ubuntu/pics/out.png")
 
     cascPath = "./haarcascade_frontalface_default.xml"
     # Create the haar cascade
@@ -19,23 +19,25 @@ def findNumberOfFaces(imageName, inputScaleFactor = 1.1, rectangle = False, size
         image,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(15, 15)
+        minSize=(175, 175)
         #flags re= cv2.CV_HAAR_SCALE_IMAGE
     )
     
     # Draw a rectangle around the faces0
     if(rectangle):
-        for (x, y, w, h) in faces:
-            #cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
-            #cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        if(len(faces)):
+          for (x, y, w, h) in faces:
+           # cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
+           # cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.imwrite("./pics/test.png", image)
+          file_name = str(len(faces)) + "_" + str(time.time())
+          cv2.imwrite("/home/phnx/echo-mirror/ubuntu/pics/" + file_name + ".png", image)
     else:
         for (x, y, w, h) in faces:
             print("FACE SIZE: " + str(w-x) + "," + str(h-y))
     m = int(round(time.time() * 1000)) - m
     print(len(faces))
-    return len(faces)
+    #sys.exit(len(faces))
 
 def changeProportionalSize(img, ratio = 0.5):
     height, width, channels = img.shape
